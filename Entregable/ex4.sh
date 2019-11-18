467206cbb04f7aff2f8c73c9398c30164f93b7d8
Agustin Calatayud <acalatayud@itba.edu.ar>
2019-11-18 18:59:22 -0300

    Blast example corrected

--- Entregable/ex4.sh
+++ Entregable/ex4.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-if [ $# -ne 2 ]
-  then echo "Input file missing or pattern"
+if [ $# -ne 1 ]
+  then echo "Input file missing"
 else
   java -cp './TP1.jar' -Dexercise=4 -Dinput=$1 -Doutput="exercise4" -Dpattern=$2 "ar.edu.itba.bio.Main"
 fi
